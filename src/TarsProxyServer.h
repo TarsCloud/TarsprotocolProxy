f0a1799e3fa4a8cf24427ad73bc0a390ed7ee6f1
Mark⚡️ <cycle44@gmail.com>
2020-09-15 20:27:30 +0800

    Update TarsProxyServer.h

--- src/TarsProxyServer.h
+++ src/TarsProxyServer.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2020
+// Linux Foundation Projects
+//       Link: https://www.linuxfoundation.org/projects/
+// TARS Foundation Projects
+//       Link: https://github.com/TarsCloud
+// All rights reserved.
+
 #ifndef _TarsProxyServer_H_
 #define _TarsProxyServer_H_
 
