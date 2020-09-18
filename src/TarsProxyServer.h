4d29e80f6e0bf392f2cfdc834254946647dc7d4b
Mark⚡️ <cycle44@gmail.com>
2020-09-18 10:53:24 +0800

    Update TarsProxyServer.h

--- src/TarsProxyServer.h
+++ src/TarsProxyServer.h
@@ -12,52 +12,49 @@
 #include "servant/Application.h"
 #include "util/tc_thread_rwlock.h"
 
-
-using namespace tars;
-
 /**
  *
  **/
-class TarsProxyServer : public Application
-{
-  public:
-    /**
-	 *
-	 **/
-    virtual ~TarsProxyServer(){};
+class TarsProxyServer : public Application {
+ public:
+  /**
+       *
+       **/
+  virtual ~TarsProxyServer() {}
 
-    /**
-	 *
-	 **/
-    virtual void initialize();
+  /**
+       *
+       **/
+  virtual void initialize();
 
-    /**
-	 *
-	 **/
-    virtual void destroyApp();
+  /**
+       *
+       **/
+  virtual void destroyApp();
 
-    string getProxyObj(const string &servant, bool &hasProxy);
+  string getProxyObj(const string &servant, bool &hasProxy);
 
-    bool checkAuth(const string &ip, const string &obj, const string &func);
+  bool checkAuth(const string &ip, const string &obj, const string &func);
 
-    //bool					_forNode;
+  // bool _forNode;
 
-  protected:
-    bool cmdLoadProxyConf(const string &command, const string &params, string &result);
+ protected:
+  bool cmdLoadProxyConf(const string &command, const string &params, string &result);
 
-    bool loadConf(bool isStartMain);
+  bool loadConf(bool isStartMain);
 
-  private:
-    bool _checkAuthOnOff;
-    map<string, set<string>> _authList; // key: obj:func,  value: ip set
-    set<string> _ipWhiteList;
-    set<string> _ipBlackList;
-    set<string> _serverWhiteList;
-    map<string, string> _proxy;
+ private:
+  bool _checkAuthOnOff;
+  map<string, set<string> > _authList;  // key: obj:func,  value: ip set
+  set<string> _ipWhiteList;
+  set<string> _ipBlackList;
+  set<string> _serverWhiteList;
+  map<string, string> _proxy;
 
-    TC_ThreadRWLocker _confRWLock;
+  TC_ThreadRWLocker _confRWLock;
 };
 
 extern TarsProxyServer g_app;
 
+////////////////////////////////////////////
 #endif
